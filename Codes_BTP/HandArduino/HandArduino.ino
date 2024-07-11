fd5af1c3ed5024983685bf333912f7953f3dea04
vyvas33 <113670456+vyvas33@users.noreply.github.com>
2024-07-11 11:18:54 +0530

    Update HandArduino.ino

--- Codes_BTP/HandArduino/HandArduino.ino
+++ Codes_BTP/HandArduino/HandArduino.ino
@@ -4,10 +4,10 @@ const int relayPins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 void setup() {
   Serial.begin(115200);
 
-  // Set relay pins as OUTPUT
+ 
   for (int i = 0; i < 10; i++) {
     pinMode(relayPins[i], OUTPUT);
-    // Initially turn off all relays
+    
     digitalWrite(relayPins[i], HIGH); // Change LOW to HIGH
   }
 }
@@ -18,48 +18,48 @@ void loop() {
 
     switch (receivedChar) {
       case '0':
-        controlRelay(0, LOW); // Change HIGH to LOW
-        controlRelay(1, LOW); // Change HIGH to LOW
-        controlRelay(2, LOW); // Change HIGH to LOW
-        controlRelay(3, LOW); // Change HIGH to LOW
-        controlRelay(4, LOW); // Change HIGH to LOW
+        controlRelay(0, LOW); 
+        controlRelay(1, LOW); 
+        controlRelay(2, LOW);
+        controlRelay(3, LOW); 
+        controlRelay(4, LOW);
         
         delay(5000); 
         turnOffRelays();
         break;
 
       case '1':
-        controlRelay(0, LOW); // Change HIGH to LOW
-        controlRelay(2, LOW); // Change HIGH to LOW
-        controlRelay(3, LOW); // Change HIGH to LOW
-        controlRelay(4, LOW); // Change HIGH to LOW
+        controlRelay(0, LOW); 
+        controlRelay(2, LOW); 
+        controlRelay(3, LOW); 
+        controlRelay(4, LOW); 
 
         delay(5000); 
         turnOffRelays();
         break;
       
       case '2':
-        controlRelay(0, LOW); // Change HIGH to LOW
-        controlRelay(3, LOW); // Change HIGH to LOW
-        controlRelay(4, LOW); // Change HIGH to LOW
+        controlRelay(0, LOW); 
+        controlRelay(3, LOW);
+        controlRelay(4, LOW); 
 
         delay(5000);
 
-        controlRelay(0, HIGH); // Change LOW to HIGH
-        controlRelay(3, HIGH); // Change LOW to HIGH
-        controlRelay(4, HIGH); // Change LOW to HIGH
+        controlRelay(0, HIGH); 
+        controlRelay(3, HIGH); 
+        controlRelay(4, HIGH); 
         break;
 
       case '3':
-        controlRelay(0, LOW); // Change HIGH to LOW
-        controlRelay(4, LOW); // Change HIGH to LOW
+        controlRelay(0, LOW); 
+        controlRelay(4, LOW);
         
         delay(5000);
         turnOffRelays();
         break;
 
       case '4':
-        controlRelay(4, LOW); // Change HIGH to LOW
+        controlRelay(4, LOW);
          
         delay(5000);
         turnOffRelays();
@@ -71,7 +71,7 @@ void loop() {
 
       case 'o':
         for (int i = 5; i < 10; i++) {
-            controlRelay(i, LOW); // Change HIGH to LOW
+            controlRelay(i, LOW); 
         }
         break;
     }
@@ -86,7 +86,7 @@ void controlRelay(int relayIndex, int state) {
 }
 
 void turnOffRelays() {
-  // Turn off all relays
+ 
   for (int i = 0; i < 10; i++) {
     digitalWrite(relayPins[i], HIGH);
   }
